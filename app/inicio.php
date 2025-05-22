e84838f7553acb161490cd36f2ab5540b2b1ce85
gwitus <emanuelbarbosati@gmail.com>
2025-05-22 16:33:04 -0300

    alteração 1.0

--- app/inicio.php
+++ app/inicio.php
@@ -9,20 +9,21 @@ session_start();
     <title>Sítio Baruk</title>
     <link href="css/inicio.css" rel="stylesheet">
 </head>
-<body>
-    <?php echo "<h1>Bem-vindo ao Sítio Baruk:  ". $_SESSION["nome"]."</h1>";?> 
-    <p>Escolha uma das opções abaixo para navegar:</p>
-    <?php if($_SESSION["permissao"]==1){
-    echo "<a href='funcionario.html'>Cadastro de Funcionário.</a>";    
-    echo "<a href='tarefa.html'>Cadastro de Tarefas.</a>";
-    echo"<a href='programacao.php'>Cadastro de Programação de Tarefas.</a>";
-    echo"<a href='usuario.php'>Cadastro de Usuários.</a>";
-    echo"<a href='consultas.php'>Consultas.</a>";
-}?>
-    <a href="coleta_prog.php">Cadastro de Coletas de Dados.</a>
-	<a href="listagemtarefas.php">Listagem de Tarefas.</a>
-    <a href="programacaoporfunc.php">Consulta de Programação por Funcionário.</a>
-    <a href="redsenha.php">Alterar senha.</a>
-	<a href="../public/login.html">Finalizar.</a>
-</body>
+    <body>
+        <?php echo "<h1>Bem-vindo ao Sítio Baruk:  ". $_SESSION["nome"]."</h1>";?> 
+        <p>Escolha uma das opções abaixo para navegar:</p>
+        <?php if($_SESSION["permissao"]==1){
+            echo "<a href='funcionario.html'>Cadastro de Funcionário.</a>";    
+            echo "<a href='tarefa.html'>Cadastro de Tarefas.</a>";
+            echo"<a href='programacao.php'>Cadastro de Programação de Tarefas.</a>";
+            echo"<a href='usuario.php'>Cadastro de Usuários.</a>";
+            echo"<a href='consultas.php'>Consultas.</a>";
+        }
+        ?>
+        <a href="coleta_prog.php">Cadastro de Coletas de Dados.</a>
+        <a href="listagemtarefas.php">Listagem de Tarefas.</a>
+        <a href="programacaoporfunc.php">Consulta de Programação por Funcionário.</a>
+        <a href="redsenha.php">Alterar senha.</a>
+        <a href="../public/login.html">Finalizar.</a>
+    </body>
 </html>
